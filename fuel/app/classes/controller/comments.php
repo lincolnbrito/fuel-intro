eab57966b8295747b8b8ad2646ec0bf360a7f28d
Lincoln Brito <lincoln.sbrito@gmail.com>
2014-10-13 14:21:53 -0300

    view to create a comment

--- fuel/app/classes/controller/comments.php
+++ fuel/app/classes/controller/comments.php
@@ -10,8 +10,31 @@ class Controller_Comments extends Controller_Template
 		$this->template->content = View::forge('comments/edit', $data);
 	}
 
-	public function action_create()
+	public function action_create($id = null)
 	{
+		if(Input::post())
+		{
+			$comment = Model_Comment::forge(array(
+				'name' => Input::post('name'),
+				'comment' => Input::post('comment'),
+				'message_id' => Input::post('message_id'),
+			));
+
+			if($comment and $comment->save())
+			{
+				Session::set_flash('success', 'Added comment #'.$comment->id.'.');
+				Response::redirect('messages/view/'.$comment->message_id);
+			}
+			else
+			{
+				Session::set_flash('error', 'Could not save comment');
+			}
+		}
+		else
+		{
+			$this->template->set_global('message', $id, false);
+		}
+
 		$data["subnav"] = array('create'=> 'active' );
 		$this->template->title = 'Comments &raquo; Create';
 		$data['form'] = View::forge('comments/_form');
--- fuel/app/views/messages/view.php
+++ fuel/app/views/messages/view.php
@@ -6,6 +6,6 @@
 <p>
 	<strong>Message:</strong>
 	<?php echo $message->message; ?></p>
-
+<p><?php echo Html::anchor('comments/create/'.$message->id, 'Add new Comment') ?></p>
 <?php echo Html::anchor('messages/edit/'.$message->id, 'Edit'); ?> |
 <?php echo Html::anchor('messages', 'Back'); ?>
