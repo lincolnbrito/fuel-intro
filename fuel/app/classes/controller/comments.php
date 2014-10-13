9ae090f25f3ce37e6710256e06d402c5fbc204e8
Lincoln Brito <lincoln.sbrito@gmail.com>
2014-10-13 13:49:52 -0300

    creating comments controller and views

---
+++ fuel/app/classes/controller/comments.php
@@ -0,0 +1,20 @@
+<?php
+
+class Controller_Comments extends Controller_Template
+{
+
+	public function action_edit()
+	{
+		$data["subnav"] = array('edit'=> 'active' );
+		$this->template->title = 'Comments &raquo; Edit';
+		$this->template->content = View::forge('comments/edit', $data);
+	}
+
+	public function action_create()
+	{
+		$data["subnav"] = array('create'=> 'active' );
+		$this->template->title = 'Comments &raquo; Create';
+		$this->template->content = View::forge('comments/create', $data);
+	}
+
+}
